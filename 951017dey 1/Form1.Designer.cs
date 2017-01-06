0ba01f60114c93b1beb9d122e7c2688271f1ae07
m-parashkhoo <mohammadrezamp@gmail.com>
2017-01-06 12:03:26 +0330

    Init Commit

---
+++ 951017dey 1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace _951017dey_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.listBoxDirectorys = new System.Windows.Forms.ListBox();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnGo);
+            this.panel1.Controls.Add(this.txtPath);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(2, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 66);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Path:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(59, 25);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(539, 22);
+            this.txtPath.TabIndex = 1;
+            this.txtPath.Text = "c:\\Windows";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(629, 16);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(105, 40);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go...";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // listBoxDirectorys
+            // 
+            this.listBoxDirectorys.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxDirectorys.FormattingEnabled = true;
+            this.listBoxDirectorys.ItemHeight = 16;
+            this.listBoxDirectorys.Location = new System.Drawing.Point(2, 66);
+            this.listBoxDirectorys.Name = "listBoxDirectorys";
+            this.listBoxDirectorys.Size = new System.Drawing.Size(775, 164);
+            this.listBoxDirectorys.TabIndex = 1;
+            this.listBoxDirectorys.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBoxDirectorys_MouseDoubleClick);
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.ItemHeight = 16;
+            this.listBoxFiles.Location = new System.Drawing.Point(2, 244);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(775, 244);
+            this.listBoxFiles.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 487);
+            this.Controls.Add(this.listBoxFiles);
+            this.Controls.Add(this.listBoxDirectorys);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxDirectorys;
+        private System.Windows.Forms.ListBox listBoxFiles;
+    }
+}
+
---
+++ 951017dey 1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _951017dey_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void DisplayDirectoriesAndFies()
+        {
+            if (string.IsNullOrWhiteSpace(txtPath.Text))
+            {
+                System.Windows.Forms.MessageBox.Show(text: "آدرس صحیح نمی باشد!.",
+                  caption: "خطا",
+                  buttons: MessageBoxButtons.OK,
+                  icon: MessageBoxIcon.Error,
+                  defaultButton: MessageBoxDefaultButton.Button1,
+                  options: MessageBoxOptions.RightAlign | MessageBoxOptions.RtlReading);
+                txtPath.Focus();
+                return;
+            }
+
+
+
+
+            System.IO.DirectoryInfo oDirectoryInfo =
+                new System.IO.DirectoryInfo(path: txtPath.Text);
+
+
+            txtPath.Text =
+                txtPath.Text.Trim();
+
+            listBoxDirectorys.Items.Clear();
+
+            if (txtPath.Text.Length > 3)
+            {
+                listBoxDirectorys.Items.Add("..");
+            }
+
+            foreach (System.IO.DirectoryInfo oDirectory in oDirectoryInfo.GetDirectories())
+            {
+                listBoxDirectorys.Items.Add(oDirectory.Name);
+            }
+
+            listBoxFiles.Items.Clear();
+
+            foreach (System.IO.FileInfo oFiles in oDirectoryInfo.GetFiles())
+            {
+                listBoxFiles.Items.Add(oFiles.Name);
+            }
+
+
+
+
+
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            DisplayDirectoriesAndFies();
+        }
+
+        private void listBoxDirectorys_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listBoxDirectorys.SelectedItem != null)
+            {
+                if (listBoxDirectorys.SelectedItem.ToString() != "..")
+                {
+                    txtPath.Text =
+                        string.Format(@"{0}\{1}", txtPath.Text, listBoxDirectorys.SelectedItem);
+                }
+                else
+                {
+                    int intIndex = txtPath.Text.LastIndexOf("\\");
+                    txtPath.Text =
+                        txtPath.Text.Substring(startIndex: 0, length: intIndex);
+                }
+
+                DisplayDirectoriesAndFies();
+            }
+
+        }
+    }
+}
